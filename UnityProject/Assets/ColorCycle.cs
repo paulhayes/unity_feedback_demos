347445a4cf382950b3daeeaf2d9ad647ac8b2caf
Paul Hayes <paul.hayes@entropedia.co.uk>
2014-07-15 08:52:09 +0100

    Initial commit

---
+++ UnityProject/Assets/ColorCycle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorCycle : MonoBehaviour 
+{
+	public Color color1;
+	public Color color2;
+	public float speed;
+	public Material mat;
+		
+	void Update ()
+	{
+		mat.color = Color.Lerp( color1, color2, Mathf.PingPong(Time.time + Random.Range(0f,0.2f),speed) );
+	}
+}
---
+++ UnityProject/Assets/Feedback.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Feedback : MonoBehaviour 
+{
+
+	Texture2D lastScreenCapture;
+	public Renderer renderQuad;
+
+	void Awake () 
+	{
+		lastScreenCapture = new Texture2D(Screen.width,Screen.height);
+		lastScreenCapture.SetPixels( lastScreenCapture.GetPixels().Select(c=>Color.black).ToArray() );
+		lastScreenCapture.Apply();
+		renderQuad.material.mainTexture = lastScreenCapture;
+		
+		float aspectRatio = 1f * Screen.width / Screen.height;
+		renderQuad.transform.localScale = new Vector3(aspectRatio,1f,1f);
+		
+	}
+	
+	void Start(){
+	
+	}
+	
+	void Update ()
+	{
+		
+	}
+	
+	void OnPostRender(){
+		if( Camera.current == Camera.main ){
+			lastScreenCapture.ReadPixels(new Rect(0,0,Screen.width,Screen.height),0,0);
+			lastScreenCapture.Apply();
+            
+		}
+	}
+}
