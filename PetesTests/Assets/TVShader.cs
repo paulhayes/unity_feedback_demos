65238d596d7f13a0d9f16771c535e3782961a02a
Pete Hobson <pete@fatpebble.com>
2014-07-17 15:30:24 +0100

    adding pets basic tests

---
+++ PetesTests/Assets/TVShader.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+
+public class TVShader : MonoBehaviour
+{
+	public Material outMaterial;
+	public Shader shader;
+	public Material Additive;
+	private Material _material;
+	[Range(0, 1)] public float verts_force = 0.0f;
+	[Range(0, 1)] public float verts_force_2 = 0.0f;
+	[Range(0, 100000)] public float distanceFalloff = 0.0f;
+	
+	protected Material material
+	{
+		get
+		{
+			if (_material == null)
+			{
+				_material = new Material(shader);
+				_material.hideFlags = HideFlags.HideAndDontSave;
+			}
+			return _material;
+		}
+	}
+	
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+
+		if (shader == null) return;
+		Material mat = material;
+
+		mat.SetFloat("_VertsColor", 1-verts_force);
+		mat.SetFloat("_VertsColor2", 1-verts_force_2);
+		mat.SetFloat("_Distance", distanceFalloff);
+		outMaterial.mainTexture = source;
+		Graphics.Blit(source, destination, mat);
+
+		//Graphics.Blit(destination, destination, mat);
+	}
+	
+	void OnDisable()
+	{
+		if (_material)
+		{
+			DestroyImmediate(_material);
+		}
+	}
+}
---
+++ PetesTests/Assets/Trip.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trip : MonoBehaviour {
+	public Texture2D tex;
+	public Material mat;
+	// Use this for initialization
+	void Start () {
+		tex = new Texture2D(Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnPostRender() {
+		Debug.Log ("hello2");
+		tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+		tex.Apply();
+		mat.mainTexture = tex;
+
+
+		/*
+		// Create a shader that renders white only to alpha channel
+		if(!mat) {
+			mat = new Material( "Shader \"Hidden/SetAlpha\" {" +
+			                   "SubShader {" +
+			                   "	Pass {" +
+			                   //"		ZTest Always Cull Off ZWrite Off" +
+			                   //"		ColorMask A" +
+			                   "		Color (1,0,0,1)" +
+			                   "	}" +
+			                   "}" +
+			                   "}"
+			                   );
+		}
+		// Draw a quad over the whole screen with the above shader
+		GL.PushMatrix ();
+		GL.LoadOrtho ();
+		for (var i = 0; i < mat.passCount; ++i) {
+			mat.SetPass (i);
+			GL.Begin( GL.QUADS );
+			GL.Vertex3( 0, 0, 0.1f );
+			GL.Vertex3( 1, 0, 0.1f );
+			GL.Vertex3( 1, 1, 0.1f );
+			GL.Vertex3( 0, 1, 0.1f );
+			GL.End();
+		}
+		GL.PopMatrix ();	
+		*/
+	}
+}
