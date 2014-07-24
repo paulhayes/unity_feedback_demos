80ef6e3d02026e560dfa8ea70be6cc8b6dd4c693
Paul Hayes <paul.hayes@entropedia.co.uk>
2014-07-24 21:25:39 +0100

    Wrote pitch occilator script for blackhold demo

---
+++ UnityProject/Assets/Scripts/PitchOccilate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PitchOccilate : MonoBehaviour 
+{
+
+	public AudioSource audioSource;
+
+	public float min;
+	public float max;
+	public float freq;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update ()
+	{
+		//0.1, 4, 0.05
+		//audioSource.pitch = Mathf.Lerp( min, max, Mathf.PingPong(Time.time,freq));
+		audioSource.pitch = Mathf.Lerp( min, max, Mathf.PingPong(freq*Time.time,1));
+	}
+}
